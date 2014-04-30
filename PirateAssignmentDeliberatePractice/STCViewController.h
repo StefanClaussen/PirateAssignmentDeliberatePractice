43832883e1acc5caaeb25987903f9807a5fc82cb
StefenClausen <stefanclaussen@me.com>
2014-04-30 16:41:41 +0100

    Connected the view to the controller.  Set up initial properties and actions.

--- PirateAssignmentDeliberatePractice/STCViewController.h
+++ PirateAssignmentDeliberatePractice/STCViewController.h
@@ -10,4 +10,22 @@
 
 @interface STCViewController : UIViewController
 
+@property (strong, nonatomic) IBOutlet UIImageView *backgroundImageView;
+@property (strong, nonatomic) IBOutlet UILabel *healthNumberLabel;
+@property (strong, nonatomic) IBOutlet UILabel *damageNumberLabel;
+@property (strong, nonatomic) IBOutlet UILabel *weaponNameLabel;
+@property (strong, nonatomic) IBOutlet UILabel *armorNameLabel;
+@property (strong, nonatomic) IBOutlet UIButton *actionButton;
+@property (strong, nonatomic) IBOutlet UILabel *storyLabel;
+@property (strong, nonatomic) IBOutlet UIButton *northButton;
+@property (strong, nonatomic) IBOutlet UIButton *eastButton;
+@property (strong, nonatomic) IBOutlet UIButton *southButton;
+@property (strong, nonatomic) IBOutlet UIButton *westButton;
+
+- (IBAction)actionButtonPressed:(UIButton *)sender;
+- (IBAction)northButtonPressed:(UIButton *)sender;
+- (IBAction)eastButtonPressed:(UIButton *)sender;
+- (IBAction)southButtonPressed:(UIButton *)sender;
+- (IBAction)westButtonPressed:(UIButton *)sender;
+
 @end
