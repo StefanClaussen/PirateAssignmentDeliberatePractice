52497ed756e363ab25f7cef6c341a6a4ef9220f8
StefenClausen <stefanclaussen@me.com>
2014-04-30 16:12:16 +0100

    Initial Commit

---
+++ PirateAssignmentDeliberatePractice/STCViewController.h
@@ -0,0 +1,13 @@
+//
+//  STCViewController.h
+//  PirateAssignmentDeliberatePractice
+//
+//  Created by Stefan Claussen on 30/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STCViewController : UIViewController
+
+@end
