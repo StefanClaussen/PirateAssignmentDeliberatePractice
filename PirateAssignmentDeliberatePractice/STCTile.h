22bfc15eb07947048f0c863884b6dd7c8b2287c5
StefenClausen <stefanclaussen@me.com>
2014-04-30 16:45:07 +0100

    Create new tile class

---
+++ PirateAssignmentDeliberatePractice/STCTile.h
@@ -0,0 +1,17 @@
+//
+//  STCTile.h
+//  PirateAssignmentDeliberatePractice
+//
+//  Created by Stefan Claussen on 30/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface STCTile : NSObject
+
+@property (strong, nonatomic)NSString *story;
+@property (strong, nonatomic)UIImage *backgroundImage;
+@property (strong, nonatomic)NSString *actionButtonName;
+
+@end
---
+++ PirateAssignmentDeliberatePractice/STCTile.m
@@ -0,0 +1,13 @@
+//
+//  STCTile.m
+//  PirateAssignmentDeliberatePractice
+//
+//  Created by Stefan Claussen on 30/04/2014.
+//  Copyright (c) 2014 One foot after the other. All rights reserved.
+//
+
+#import "STCTile.h"
+
+@implementation STCTile
+
+@end
